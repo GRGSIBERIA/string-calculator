d0b8a4a18ea80d097637fd3b7c5e57045d60b9c8
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-12-25 19:53:56 +0900

    電卓づくり

--- web/index.dart
+++ web/index.dart
@@ -3,4 +3,7 @@ import 'dart:html';
 void main() {
   var message = new DivElement()..text = "hoge";
   document.body.append(message);
+  
+  ButtonElement button = document.querySelector("#calculate-btn");
+  button..onClick.listen((_) => print("hogehoge"));
 }
