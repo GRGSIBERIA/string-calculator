350e98fd7517310f8ed746be65941d70d809a451
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-12-25 23:03:47 +0900

    計算する

--- web/compute.dart
+++ web/compute.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math';
 
 
 double getValueOnInput(String idname) {
@@ -12,17 +13,42 @@ double getValueOnInput(String idname) {
   return value;
 }
 
+void recordValueOnInput(double value, String idname) {
+  InputElement input = document.querySelector(idname) as InputElement;
+  try {
+    input.value = value.toString();
+  } catch (exception) {
+    window.alert("計算できませんでした：" + idname);
+  }
+}
+
 void writeConsole(String text) {
   var message = new DivElement()..text = text;
   document.body.append(message);
 }
 
 void computeString() {
-  double len0 = getValueOnInput('#length');
-  double diameter = getValueOnInput('#diameter');
-  double freq = getValueOnInput('#frequency');
-  double density = getValueOnInput('#density');
-  double young = getValueOnInput('#youngs-modulus');
+  final double len0 = getValueOnInput('#length');
+  final double diameter = getValueOnInput('#diameter');
+  final double frequency = getValueOnInput('#frequency');
+  final double density = getValueOnInput('#density');
+  final double young = getValueOnInput('#youngs-modulus');
+
+  final double crossSection = pow(diameter/2.0, 2.0) * pi;
+  final double stringDensity = crossSection * density;  
+  final double stress = 4.0 * pow(len0 * frequency, 2.0) * density;
+  final double tension = stress * crossSection;
+  final double strain = stress / young;
+  final double stretch = (len0 * stress) / young;
+  final double reallyLength = len0 + stretch;
+
+  recordValueOnInput(crossSection, '#cross-section');
+  recordValueOnInput(stringDensity, '#string-density');
+  recordValueOnInput(stress, '#stress');
+  recordValueOnInput(tension, '#tension');
+  recordValueOnInput(strain, '#strain');
+  recordValueOnInput(stretch, '#stretch');
+  recordValueOnInput(reallyLength, '#really-length');
 
   writeConsole("Computed results.");
 }
