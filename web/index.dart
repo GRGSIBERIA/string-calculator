74a2f93c21405a23ce7f1aca2894619fab0ecfd5
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-12-25 20:32:21 +0900

    dartで実行できるようになった

--- web/index.dart
+++ web/index.dart
@@ -1,9 +1,14 @@
 import 'dart:html';
 
+void calculate() {
+  var len0 = document.querySelector('#length');
+  print(len0);
+}
+
 void main() {
-  var message = new DivElement()..text = "hoge";
+  var button = document.querySelector("#calculate-btn");
+  button..onClick.listen((_) => calculate());
+
+  var message = new DivElement()..text = "Loaded dart scripts.";
   document.body.append(message);
-  
-  ButtonElement button = document.querySelector("#calculate-btn");
-  button..onClick.listen((_) => print("hogehoge"));
 }
